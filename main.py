deb875c1248735ee7d36b11aa1744869832ca491
EchterAlsFakeX <117779106+EchterAlsFakeX@users.noreply.github.com>
2022-11-15 18:09:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Der Beweis dafür, dass jede ungerade Zahl + eine ungerade Zahl eine gerade Zahl ergibt.
+#
+# This program will test if a number can be divided by 2. This is just a school project for my teacher ^^
+def Arithmetisches_Mittel():
+
+
+    for i in range(1, 10000000000000000000000000000000000000000000000000000000000000000000000000000000000): #You can enter any number except 0, Pi and other weird numbers.
+        i = i + 1
+        print(i)
+        if i % 2 == 0:
+            print("True")
+        else:
+            print("False")
+
+Arithmetisches_Mittel()
